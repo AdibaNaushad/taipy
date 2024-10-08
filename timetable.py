13fd2d5b6946a8547cf61096f8180dccc47b315e
AdibaNaushad <adibanaushad0@gmail.com>
2024-10-08 19:16:32 +0530

    Create timetable.py
    current time table

---
+++ timetable.py
@@ -0,0 +1,17 @@
+# importing datetime module for now()
+import datetime
+    
+# using now() to get current time
+current_time = datetime.datetime.now()
+    
+# Printing attributes of now().
+print ("The attributes of now() are : ")
+    
+print ("Year: ", end = "")
+print (current_time.year)
+    
+print ("Month: ", end = "")
+print (current_time.month)
+    
+print ("Day: ", end = "")
+print (current_time.day)
